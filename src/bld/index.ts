5a625058cd30bef805cb2b4ce2c08b9b5dc6f7c3
Alex Thompson <pierogitus@hotmail.com>
2021-12-15 23:27:49 -0800

    bld

--- src/bld/index.ts
+++ src/bld/index.ts
@@ -1,13 +1,6 @@
-export const enum builtInList {
-    uint,
+export const enum registry {
     core,
-    links,
-    noop,
     unicode,
-    wikidataP,
-    wikidataQ,
-    wikidataL,
-    private,
 }
 export const enum core {
     linkIndex,
@@ -23,24 +16,30 @@ export const enum core {
     biasedExponentBase10,
     biasedExponentWithSignBase10,
 }
-export const enum serialOp {
+export const enum op {
+    magicNumber, //0bin
+    setOpList, //(i:number) default:0
+    noop, //()
     setStringPrefixIndex, //(i:number) default:0
     setStringPrefix, //(byteLength:number, bytes:utf8)
     setStringPrefixHttp, //(byteLength:number, bytes:utf8)
     setStringPrefixHttps, //(byteLength:number, bytes:utf8)
     extendStringPrefix, //(byteLength:number, bytes:utf8)
     shortenStringPrefix, //(byteLength:number)
-    setDestinationList, //(id:number) default:1
-    setSourceList, //(id:number) default:0
-    incrementDestinationList, //()
-    append_suffixes_to_list, //(count:number, [byteLength:number, bytes:utf8][]) append to current destination using current prefix
+    setRegistryIndex, //(i:number) default:0
+    setDestinationList, //(i:number) default:0
+    setSourceList, //(i:number) default:0
+    incrementDestinationList, //(size:number)
+    append_prefix_suffixes, //(count:number, [byteLength:number, bytes:utf8][])
     append_prefix_range, //(offset:number, runCount:number)
+    append_from_registry, //(offset:number, runCount:number)
     append_from_sourceList, //(offset:number, runCount:number)
     append_Uint, //(offset:number, bits:number)
-    append_UintNeg, //(offset:number, bits:number) 
+    append_UintNeg, //(offset:number, bits:number)
+    append_sha256, //(count:number, btyes:count*32)
+    load_link, //(i:number)
+    save_config, //(id:number)
+    load_config, //(id:number)
 }
-export const enum op {
-    add
-}
-export type constructedList = { builtIn: builtInList, offset: number, runCount: number }[]
+export type constructedList = { builtIn: any, offset: number, runCount: number }[]
 export type pack = { lists: constructedList[], count: number }
