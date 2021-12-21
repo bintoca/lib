c8e6f4cef5ebc80e388129f2c1d2e3e2174559f7
Alex Thompson <pierogitus@hotmail.com>
2021-12-20 19:39:11 -0800

    bld

--- src/bld/index.ts
+++ src/bld/index.ts
@@ -1,75 +1,56 @@
 export const enum op {
-    setSourcePlane, //(i:vint)
-    setDestinationPlane, //(i:vint)
+    setStandard_int, //()
+    setStandard_registry, //()
+    setStandard_unicode, //()
+    setStandard_sha256, //()
     setSourceList, //(i:vint)
-    setDestinationList, //(i:vint)
     addSourceList, //(i:vint)
-    subtractSourceList, //(i:vint)
+    subSourceList, //(i:vint)
+    setDestinationList, //(i:vint)
     addDestinationList, //(i:vint)
-    subtractDestinationList, //(i:vint)
-    append_offset_range, //(i:vint)
+    subDestinationList, //(i:vint)
+
+    append_single, //(i:vint)
     append_many, //(count:vint, i...)
     append_many_size, //(count:vint, size:vint, bytes:u8[])
     append_many_sizes, //(count:vint, [size:vint,bytes:u8[]]...)
-    setComponentType, //(i:vint)
-    special, //D_13
+    setSourceOffset_append_range, //(off:vint, range:vint)
+    addSourceOffset_append_range, //(off:vint, range:vint)
+    subSourceOffset_append_range, //(off:vint, range:vint)
+    append_list_of_uint, //(bits:vint)
+    append_list_of_sint, //(bits:vint)
+    append_list_of_IEEE754_binary, //(bits:vint)
+    append_list_of_IEEE754_decimal_BID, //(bits:vint)
+    append_list_of_IEEE754_decimal_DPD, //(bits:vint)
+
+    setComponent_OID, //() ASN.1 BER rules
+    setComponent_scheme, //()
+    setComponent_user, //()
+    setComponent_DNS, //()
+    setComponent_IPv6, //()
+    setComponent_IPv4, //()
+    setComponent_port, //()
+    setComponent_path, //()
+    setComponent_query, //()
+    setComponent_fragment, //()
+    setComponent_language, //()
     extendComponent, //(size:vint, bytes:u8[])
     shortenComponent, //(count:vint)
-}
-export const enum D_13 {
-    noop, //()
-    garbageCollect, //() dest -> src
-    setComponentIANAPrivateEnterpriseOID, //() 1.3.6.1.4.1
-    draftRevision, //(i:vint)  
-    setOffset, //(i:vint)
-    addOffset, //(i:vint)
-    subtractOffset, //(i:vint)
-    append_single, //(i:vint)
-    byteString, //(size:vint, bytes:u8[])
-    magicNumber, //always 7 bytes //0xD9D9F8 (non-unicode bytes derived from cbor tag 55800) + 0x42494E4C44 ("BINLD") or ("BXXLD") for beta
     extendComponent_bits, //(count:vint, bits:vint)
     shortenComponent_bits, //(count:vint)
+
+    byteString, //(size:vint, bytes:u8[])
+    garbageCollect, //() dest -> src
+
     extendedOps0param, //(i:vint)
     extendedOps1param, //(i:vint, p1:vint)
-    extendedOps2param, //(i:vint, p1:vint, p2:vint)
     extendedOpsNparam, //(i:vint, n:vint, p...)
-}
-export const enum extendedOps0param {
-}
-export const enum extendedOps1param {
-}
-export const enum mode {
-}
-export const enum plane {
-    standard,
-    sha,
-    constructed,
-    pack,
-    context,
-}
-export const enum standard {
-    int,
-    registry,
-    unicode,
-    component,
-}
-export const enum sha {
-    sha256
-}
-export const enum component {
-    ObjectIdentifier, //ASN.1 BER rules [X.690] [RFC6256] 
-    scheme,
-    DNS,
-    pathHierarchy,
-    IPv6,
-
-    pathString = 8,
-    query,
-    fragment,
-    port,
-    user,
-    IPv4,
-    language,
+    extendedOps0paramPrivate, //(i:vint)
+    extendedOps1paramPrivate, //(i:vint, p1:vint)
+    extendedOpsNparamPrivate, //(i:vint, n:vint, p...)
+    magicNumber, //always 7 bytes //0xD9D9F8 (non-unicode bytes derived from cbor tag 55800) + 0x42494E4C44 ("BINLD") or ("BXXLD") for beta
+    draftRevision, //(i:vint)
+    majorVersion, //(i:vint)
 }
 export const enum registryID {
     id,
